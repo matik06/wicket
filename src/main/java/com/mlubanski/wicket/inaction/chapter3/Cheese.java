1018b002ac5f0176654b3ccfda6854c3ec2ee31b
Mateusz Lubański <m.lubanskii@gmail.com>
2012-09-19 12:46:55 +0200

    cheeser application from Wicekt In Action

---
+++ src/main/java/com/mlubanski/wicket/inaction/chapter3/Cheese.java
@@ -0,0 +1,30 @@
+package com.mlubanski.wicket.inaction.chapter3;
+
+import java.io.Serializable;
+
+public class Cheese implements Serializable {
+    
+  private String name;
+  private String description;
+  private double price;
+  
+  public Cheese(String name, String description, double price) {
+    super();
+    this.name = name;
+    this.description = description;
+    this.price = price;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public String getDescription() {
+    return description;
+  }
+  
+  public double getPrice() {
+    return price;
+  }
+  
+}
---
+++ src/main/java/com/mlubanski/wicket/inaction/chapter3/CheesrPage.java
@@ -0,0 +1,21 @@
+package com.mlubanski.wicket.inaction.chapter3;
+
+import java.util.List;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public abstract class CheesrPage extends WebPage {
+    
+  public CheesrSession getCheesrSession() {
+    return (CheesrSession) getSession();
+  }
+
+  public Cart getCart() {
+    return getCheesrSession().getCart();
+  }
+
+  public List<Cheese> getCheeses() {
+    return CheesrApplication.get().getCheeses();
+  }
+  
+}
